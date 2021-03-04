315cfd13474ae1b6de06ec2b17b4ac0808d7a37c
Nikola Mitic <nikolamitic@nikolas-mbp.localdomain>
2021-03-04 15:11:05 +0100

    added code explanaitons comments for better understanding of server side style rendering and pageView tracking events

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,19 @@
 import {useEffect} from 'react';
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 import trackingService from '../services/trackingService';
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter()
 
   useEffect(() => {
+    // Removing server side injected styles on the client, needed for server side styles to work properly
+    // @see: https://material-ui.com/guides/server-rendering/
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
-
+  // Page View Tracking even will be fired on route change and on initial page load
   useEffect(() => {
     trackingService.pageView({url: router.pathname})
   }, [router.pathname]);
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,6 +3,7 @@ import { ServerStyleSheets } from '@material-ui/core/styles';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    // Render app and page and get the context of the page with collected side effects.
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
@@ -11,7 +12,14 @@ class MyDocument extends Document {
     });
 
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps, styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()] }
+    // Styles fragment is rendered after the app and page rendering finish.
+    return { 
+      ...initialProps,
+      styles:[
+        ...React.Children.toArray(initialProps.styles), 
+          sheets.getStyleElement()
+        ] 
+    }
   }
 
   render() {
